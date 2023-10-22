247f69d48117df9850b18daecc514972bb4e2bda
Doru Dumitru <doru.g.dumitru@gmail.com>
2023-10-22 13:30:33 +0300

    update .gitignore

---
+++ nvim/.config/nvim/lua/plugins/core.lua
@@ -0,0 +1,52 @@
+return {
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = function()
+				require("onedark").setup({
+					code_style = {
+						comments = "italic",
+						keywords = "italic",
+						functions = "bold",
+						strings = "none",
+						variables = "none",
+					},
+				})
+				require("onedark").load()
+			end,
+		},
+	},
+	{
+		{
+			"nvim-treesitter/nvim-treesitter",
+			opts = function(_, opts)
+				vim.list_extend(opts.ensure_installed, {
+					"svelte",
+				})
+			end,
+		},
+	},
+	{
+		{
+			"nvim-neo-tree/neo-tree.nvim",
+			opts = {
+				filesystem = {
+					filtered_items = {
+						hide_dotfiles = false,
+						never_show = {
+							".DS_Store",
+							".git",
+						},
+					},
+				},
+			},
+		},
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		event = "VeryLazy",
+		opts = function(_, opts)
+			opts.sections.lualine_z = {}
+		end,
+	},
+}
