9bcfbb86214189c4099ed9ef8796ad0f6490202e
Doru Dumitru <doru.dumitru@hartehanks.com>
2023-10-30 17:27:24 +0200

    updates

---
+++ nvim/.config/nvim/lua/plugins/nvim-colorizer.lua
@@ -0,0 +1,12 @@
+return {
+	-- {
+	-- 	"NvChad/nvim-colorizer.lua",
+	-- 	opts = {
+	-- 		filetypes = { "*" },
+	-- 		user_default_options = {
+	-- 			mode = "background",
+	-- 			tailwind = true,
+	-- 		},
+	-- 	},
+	-- },
+}
