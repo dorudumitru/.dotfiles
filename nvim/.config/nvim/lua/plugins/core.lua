40f929591bb7aa3ebea26867e47e3e51a653e76c
Doru Dumitru <doru.dumitru@hartehanks.com>
2024-01-26 17:55:50 +0200

    changes

--- nvim/.config/nvim/lua/plugins/core.lua
+++ nvim/.config/nvim/lua/plugins/core.lua
@@ -9,6 +9,13 @@ return {
 	},
 	{
 		"folke/noice.nvim",
+		opts = {
+			lsp = {
+				hover = {
+					silent = true,
+				},
+			},
+		},
 		keys = {
 			{ "<c-f>", false, mode = { "i", "n", "s" } },
 			{ "<c-b>", false, mode = { "i", "n", "s" } },
