a05d9191a1e4a0e83e6fc5644ed2eb89e69281f2
Doru Dumitru <doru.dumitru@hartehanks.com>
2024-02-02 10:16:49 +0200

    changes

--- nvim/.config/nvim/lua/plugins/core.lua
+++ nvim/.config/nvim/lua/plugins/core.lua
@@ -89,10 +89,21 @@ return {
 				highlighters = {
 					hex_color = hi.gen_highlighter.hex_color({ priority = 2000 }),
 					hsl_color = {
-						pattern = "hsl%(%d*%.?%d+ %d*%.?%d+%% %d*%.?%d+%%%)",
+						pattern = "hsl%(%d*%.?%d+,? %d*%.?%d+%%,? %d*%.?%d+%%%)",
 						group = function(_, match)
 							local util = require("util.color")
-							local h, s, l = match:match("hsl%((%d*%.?%d+) (%d*%.?%d+)%% (%d*%.?%d+)%%%)")
+							local h, s, l = match:match("hsl%((%d*%.?%d+),? (%d*%.?%d+)%%,? (%d*%.?%d+)%%%)")
+							h, s, l = tonumber(h), tonumber(s), tonumber(l)
+							local hex_color = util.hslToHex(h, s, l)
+							return MiniHipatterns.compute_hex_color_group(hex_color, "bg")
+						end,
+					},
+					hsla_color = {
+						pattern = "hsl%(%d*%.?%d+,? %d*%.?%d+%%,? %d*%.?%d+%%%,? %d*%.?%d+%)",
+						group = function(_, match)
+							local util = require("util.color")
+							local h, s, l =
+								match:match("hsl%((%d*%.?%d+),? (%d*%.?%d+)%%,? (%d*%.?%d+)%%%,? (%d*%.?%d+%))")
 							h, s, l = tonumber(h), tonumber(s), tonumber(l)
 							local hex_color = util.hslToHex(h, s, l)
 							return MiniHipatterns.compute_hex_color_group(hex_color, "bg")
