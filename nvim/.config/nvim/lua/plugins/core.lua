f4087a8c38d8b4a2ea051291eac4ff35056dc430
Doru Dumitru <doru.dumitru@hartehanks.com>
2024-02-22 20:15:06 +0200

    changes

--- nvim/.config/nvim/lua/plugins/core.lua
+++ nvim/.config/nvim/lua/plugins/core.lua
@@ -1,116 +1,120 @@
 return {
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = function()
-				require("onedark").load()
-			end,
-		},
-	},
-	{
-		"folke/noice.nvim",
-		opts = {
-			lsp = {
-				hover = {
-					silent = true,
-				},
-			},
-		},
-		keys = {
-			{ "<c-f>", false, mode = { "i", "n", "s" } },
-			{ "<c-b>", false, mode = { "i", "n", "s" } },
-		},
-	},
-	{
-		"nvim-treesitter/nvim-treesitter",
-		opts = function(_, opts)
-			vim.list_extend(opts.ensure_installed, {
-				"svelte",
-				"css",
-			})
-		end,
-	},
-	{
-		"williamboman/mason.nvim",
-		opts = {
-			ensure_installed = {
-				"css-lsp",
-				"emmet-language-server",
-				"lua-language-server",
-				"svelte-language-server",
-			},
-		},
-	},
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		opts = {
-			filesystem = {
-				filtered_items = {
-					hide_dotfiles = false,
-					hide_gitignored = true,
-					always_show = {
-						".gitignore",
-						".vscode",
-						".idea",
-					},
-					never_show = {
-						".git",
-					},
-				},
-			},
-			window = {
-				mappings = {
-					["Z"] = "expand_all_nodes",
-				},
-			},
-		},
-	},
-	{
-		"nvim-lualine/lualine.nvim",
-		event = "VeryLazy",
-		opts = function(_, opts)
-			opts.sections.lualine_z = {}
-		end,
-	},
-	{
-		"echasnovski/mini.hipatterns",
-		opts = function()
-			local hi = require("mini.hipatterns")
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = function()
+        require("onedark").load()
+      end,
+    },
+  },
+  {
+    "folke/noice.nvim",
+    opts = {
+      lsp = {
+        hover = {
+          silent = true,
+        },
+      },
+    },
+    keys = {
+      { "<c-f>", false, mode = { "i", "n", "s" } },
+      { "<c-b>", false, mode = { "i", "n", "s" } },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "svelte",
+        "css",
+      })
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "css-lsp",
+        "emmet-language-server",
+        "lua-language-server",
+        "svelte-language-server",
+      },
+    },
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          -- always_show = {
+          --   ".gitignore",
+          --   ".vscode",
+          --   ".idea",
+          --   ".env",
+          --   ".env.local",
+          --   ".env.development",
+          --   ".env.production",
+          -- },
+          never_show = {
+            ".git",
+          },
+        },
+      },
+      window = {
+        mappings = {
+          ["Z"] = "expand_all_nodes",
+        },
+      },
+    },
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      opts.sections.lualine_z = {}
+    end,
+  },
+  {
+    "echasnovski/mini.hipatterns",
+    opts = function()
+      local hi = require("mini.hipatterns")
 
-			return {
-				-- custom LazyVim option to enable the tailwind integration
-				tailwind = {
-					enabled = true,
-					ft = { "typescriptreact", "javascriptreact", "css", "javascript", "typescript", "html", "pcss" },
-					-- full: the whole css class will be highlighted
-					-- compact: only the color will be highlighted
-					style = "full",
-				},
-				highlighters = {
-					hex_color = hi.gen_highlighter.hex_color({ priority = 2000 }),
-					hsl_color = {
-						pattern = "hsl%(%d*%.?%d+,? %d*%.?%d+%%,? %d*%.?%d+%%%)",
-						group = function(_, match)
-							local util = require("util.color")
-							local h, s, l = match:match("hsl%((%d*%.?%d+),? (%d*%.?%d+)%%,? (%d*%.?%d+)%%%)")
-							h, s, l = tonumber(h), tonumber(s), tonumber(l)
-							local hex_color = util.hslToHex(h, s, l)
-							return MiniHipatterns.compute_hex_color_group(hex_color, "bg")
-						end,
-					},
-					hsla_color = {
-						pattern = "hsl%(%d*%.?%d+,? %d*%.?%d+%%,? %d*%.?%d+%%%,? %d*%.?%d+%)",
-						group = function(_, match)
-							local util = require("util.color")
-							local h, s, l =
-								match:match("hsl%((%d*%.?%d+),? (%d*%.?%d+)%%,? (%d*%.?%d+)%%%,? (%d*%.?%d+%))")
-							h, s, l = tonumber(h), tonumber(s), tonumber(l)
-							local hex_color = util.hslToHex(h, s, l)
-							return MiniHipatterns.compute_hex_color_group(hex_color, "bg")
-						end,
-					},
-				},
-			}
-		end,
-	},
+      return {
+        -- custom LazyVim option to enable the tailwind integration
+        tailwind = {
+          enabled = true,
+          ft = { "typescriptreact", "javascriptreact", "css", "javascript", "typescript", "html", "pcss" },
+          -- full: the whole css class will be highlighted
+          -- compact: only the color will be highlighted
+          style = "full",
+        },
+        highlighters = {
+          hex_color = hi.gen_highlighter.hex_color({ priority = 2000 }),
+          hsl_color = {
+            pattern = "hsl%(%d*%.?%d+,? %d*%.?%d+%%,? %d*%.?%d+%%%)",
+            group = function(_, match)
+              local util = require("util.color")
+              local h, s, l = match:match("hsl%((%d*%.?%d+),? (%d*%.?%d+)%%,? (%d*%.?%d+)%%%)")
+              h, s, l = tonumber(h), tonumber(s), tonumber(l)
+              local hex_color = util.hslToHex(h, s, l)
+              return MiniHipatterns.compute_hex_color_group(hex_color, "bg")
+            end,
+          },
+          hsla_color = {
+            pattern = "hsl%(%d*%.?%d+,? %d*%.?%d+%%,? %d*%.?%d+%%%,? %d*%.?%d+%)",
+            group = function(_, match)
+              local util = require("util.color")
+              local h, s, l =
+                  match:match("hsl%((%d*%.?%d+),? (%d*%.?%d+)%%,? (%d*%.?%d+)%%%,? (%d*%.?%d+%))")
+              h, s, l = tonumber(h), tonumber(s), tonumber(l)
+              local hex_color = util.hslToHex(h, s, l)
+              return MiniHipatterns.compute_hex_color_group(hex_color, "bg")
+            end,
+          },
+        },
+      }
+    end,
+  },
 }
--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,35 +4,36 @@ onedark.cursor_bg = "#61afef"
 onedark.cursor_fg = "#282c34"
 
 return {
-	color_schemes = {
-		["OneDark"] = onedark,
-	},
-	color_scheme = "OneDark",
-	font = wezterm.font_with_fallback({
-		{ family = "JetBrainsMono Nerd Font", weight = "Medium" },
-	}),
-	font_size = 13,
-	line_height = 1.1,
-	window_decorations = "RESIZE",
-	use_fancy_tab_bar = false,
-	initial_cols = 110,
-	initial_rows = 25,
-	window_padding = {
-		left = 0,
-		right = 0,
-		top = 0,
-		bottom = 0,
-	},
-	keys = {
-		{
-			key = "H",
-			mods = "ALT",
-			action = wezterm.action.DisableDefaultAssignment,
-		},
-		{
-			key = "L",
-			mods = "ALT",
-			action = wezterm.action.DisableDefaultAssignment,
-		},
-	},
+  front_end = "WebGpu",
+  color_schemes = {
+    ["OneDark"] = onedark,
+  },
+  color_scheme = "OneDark",
+  font = wezterm.font_with_fallback({
+    { family = "JetBrainsMono Nerd Font", weight = "Medium" },
+  }),
+  font_size = 14,
+  line_height = 1.1,
+  window_decorations = "RESIZE",
+  use_fancy_tab_bar = false,
+  initial_cols = 110,
+  initial_rows = 25,
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+  },
+  keys = {
+    {
+      key = "H",
+      mods = "ALT",
+      action = wezterm.action.DisableDefaultAssignment,
+    },
+    {
+      key = "L",
+      mods = "ALT",
+      action = wezterm.action.DisableDefaultAssignment,
+    },
+  },
 }
