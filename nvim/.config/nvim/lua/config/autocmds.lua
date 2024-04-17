db6f7c90ea51af71a0fce8785d43fc6b7ddf05d3
Doru Dumitru <doru.dumitru@hartehanks.com>
2024-04-17 16:55:24 +0300

    changes

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,27 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+	pattern = { "*.go" },
+	callback = function()
+		local lsputil = require("lspconfig.util")
+		local cwd = lsputil.root_pattern("go.mod")(vim.fn.expand("%:p"))
+		local config = lsputil.root_pattern(".golangci.yaml")(vim.fn.expand("%:p"))
+		local golangcilint = require("lint.linters.golangcilint")
+		golangcilint.args = {
+			"run",
+			"--fast",
+			"--out-format",
+			"json",
+			"--timeout",
+			"5m",
+		}
+		if config ~= nil then
+			config = config .. "/.golangci.yaml"
+			table.insert(golangcilint.args, "--config")
+			table.insert(golangcilint.args, config)
+		end
+		golangcilint.cwd = cwd
+	end,
+})
