f459a7004c5c2b3b33e6989c72e6b66e6354825d
dorudumitru-hh <doru.dumitru@hartehanks.com>
2025-02-12 09:49:34 +0200

    changes

--- stow.sh
+++ stow.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 for dir in */; do # list directories in the form "dirname/"
-	if [ $dir == "nvim.vscode/" ]; then
-		continue
-	fi
+  if [ $dir == "vscode-keybindings/" ]; then
+    rm $HOME/.config/Code/User/keybindings.json
+  elif [[ $dir == "vscode-settings/" ]]; then
+    rm $HOME/.config/Code/User/settings.json
+  fi
 
-	dir=${dir%*/}     # remove the trailing "/"
-	stow "${dir##*/}" # run stow for each directory
+  dir=${dir%*/}     # remove the trailing "/"
+  stow "${dir##*/}" # run stow for each directory
 done
