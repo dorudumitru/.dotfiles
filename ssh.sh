5d95b38168099996813e3bb9d0822e70a581df6b
Doru Dumitru <doru.g.dumitru@gmail.com>
2026-09-21 21:52:08 +0300

    removed .zshrc

--- ssh.sh
+++ ssh.sh
@@ -2,16 +2,13 @@
 
 mkdir -p "$HOME"/.ssh
 
-rm "$HOME"/.ssh/hh_enterprise.pub
-rm "$HOME"/.ssh/work.pub
+# rm "$HOME"/.ssh/work.pub
 rm "$HOME"/.ssh/personal.pub
 
-ssh-add -L | grep "HH-Enterprise" >"$HOME"/.ssh/hh_enterprise.pub
-ssh-add -L | grep "Work" >"$HOME"/.ssh/work.pub
+# ssh-add -L | grep "Work" >"$HOME"/.ssh/work.pub
 ssh-add -L | grep "Personal" >"$HOME"/.ssh/personal.pub
 
-chmod 644 "$HOME"/.ssh/hh_enterprise.pub
-chmod 644 "$HOME"/.ssh/work.pub
+# chmod 644 "$HOME"/.ssh/work.pub
 chmod 644 "$HOME"/.ssh/personal.pub
 
 cd ~/.dotfiles && git remote set-url origin git@github.com:dorudumitru/.dotfiles.git
