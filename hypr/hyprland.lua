e1c97c5d6fc8435f46f16439421af99384c04933
Doru Dumitru <doru.g.dumitru@gmail.com>
2026-09-24 10:53:07 +0300

    remove unused font in kitty and unused property in hyprland

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -81,12 +81,6 @@ hl.window_rule({ match = { class = "^(zoom)$" }, float = true })
 hl.layer_rule({ match = { namespace = "^(quickshell)$" }, no_anim = true })
 hl.layer_rule({ match = { namespace = "^dms:.*" }, no_anim = true })
 
-hl.config({
-	xwayland = {
-		force_zero_scaling = true,
-	},
-})
-
 require("dms.colors")
 require("dms.outputs")
 require("dms.layout")
